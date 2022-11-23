f4abb4625c6a9fc3756e4a6294d2528b1df6a0ef
cracklesparkle <8038570+cracklesparkle@users.noreply.github.com>
2022-11-23 16:12:15 +0900

    Quiz component

---
+++ src/quizData.js
@@ -0,0 +1,324 @@
+export const quizData1 = {
+    "questions": [
+      {
+        "question": "Самоназвание юкагиров:",
+        "questionType": "text",
+        "answerSelectionType": "multiple",
+        "answers": [
+          "алайи",
+          "wадулы",
+          "когимэ",
+          "одулы"
+        ],
+        "correctAnswer": [2, 4]
+      },
+      {
+        "question": "Их языки в науке известны как:",
+        "questionType": "text",
+        "answerSelectionType": "multiple",
+        "answers": [
+          "тундренный юкагирский язык",
+          "нижнеколымский юкагирский язык",
+          "верхнеколымский юкагирский язык",
+          "лесной юкагирский язык"
+        ],
+        "correctAnswer": [1, 4]
+      },
+      {
+        "question": "Традиционные занятия юкагиров:",
+        "questionType": "text",
+        "answers": [
+          "оленеводство, коневодство",
+          "оленеводство, рыболовство",
+          "рыболовство, коневодство"
+        ],
+        "correctAnswer": "2"
+      }
+    ],
+    "quizTitle": "1"
+  }
+  
+  export const quizData2 = {
+    "questions": [
+      {
+        "question": "Как будет «Добрый день» на юкагирском языке:",
+        "questionType": "text",
+        "answers": [
+          "амучэ чайлэлэк",
+          "амучэ чиҥичэлэк",
+          "амучэ идьигойгирлэк",
+          "Амучэ аwйааҕарлэк!"
+        ],
+        "correctAnswer": "1"
+      },
+      {
+        "question": "Как переводится “Тэт кирийэ кинэк?»",
+        "questionType": "text",
+        "answers": [
+          "сколько тебе лет?",
+          "как твоя фамилия?",
+          "Как тебя зовут?",
+          "в каком классе ты учишься?"
+        ],
+        "correctAnswer": "3"
+      },
+      {
+        "question": "Как переводится “До свидания!”:",
+        "questionType": "text",
+        "answers": [
+          "Иилугульэ мэньк!",
+          "Ньийуолгитньэр",
+          "Амучэ чиҥичэлэк!",
+          "Хабун сукунмольҕалньэк"
+        ],
+        "correctAnswer": "2"
+      }
+    ],
+    "quizTitle": "2"
+  }
+  
+  export const quizData3 = {
+    "questions": [
+      {
+        "question": "Как переводится слово «мама»:",
+        "questionType": "text",
+        "answers": [
+          "амаа", 
+          "эньиэ", 
+          "экыа", 
+          "акаа!"
+        ],
+        "correctAnswer": "2"
+      },
+      {
+        "question": "Как переводится слово «папа»:",
+        "questionType": "text",
+        "answers": [
+          "эньиэ", 
+          "экыа", 
+          "амаа", 
+          "акаа"
+        ],
+        "correctAnswer": "3"
+      },
+      {
+        "question": "Хавдьаа – это младший брат мамы?",
+        "questionType": "text",
+        "answers": [
+          "нет",
+          "да"
+        ],
+        "correctAnswer": "1"
+      },
+      {
+        "question": "Как переводится слово «абучиэ»:",
+        "questionType": "text",
+        "answers": [
+          "дедушка", 
+          "старший брат папы", 
+          "папа", 
+          "бабушка"
+        ],
+        "correctAnswer": "4"
+      }
+    ],
+    "quizTitle": "3"
+  }
+  
+  export const quizData4 = {
+    "questions": [
+      {
+        "question": "Как переводится слово «земля»:",
+        "questionType": "text",
+        "answers": ["лукул", "лаwйэ", "саал", "чаwул!"],
+        "correctAnswer": "1"
+      },
+      {
+        "question": "Как переводится слово «саал»:",
+        "questionType": "text",
+        "answers": ["трава", "море", "дерево", "цветок"],
+        "correctAnswer": "3"
+      },
+      {
+        "question": "Как переводится слово «берёза»:",
+        "questionType": "text",
+        "answers": ["чаwул", "йаа", "саал", "ульэгэ"],
+        "correctAnswer": "2"
+      },
+      {
+        "question": "Как переводится слово «хомоньэй пуриэ»:",
+        "questionType": "text",
+        "answers": ["шикша", "смородина", "брусника", "голубика"],
+        "correctAnswer": "4"
+      }
+    ],
+    "quizTitle": "4"
+  }
+  
+  export const quizData5 = {
+    "questions": [
+      {
+        "question": "Как переводится слово «животные»:",
+        "questionType": "text",
+        "answers": ["йэлукунугурчэндьэрукунпэ", "ньаwньиклиэ", "өркөбө", "сальил"],
+        "correctAnswer": "1"
+      },
+      {
+        "question": "Как переводится слово «олень»:",
+        "questionType": "text",
+        "answers": ["сальил", "хайчиэтэгэ", "илэ", "надэ"],
+        "correctAnswer": "3"
+      },
+      {
+        "question": "Как переводится слово «уйэньэйрукунпэ»:",
+        "questionType": "text",
+        "answers": ["животные", "птицы", "крыло", "клюв"],
+        "correctAnswer": "2"
+      },
+      {
+        "question": "Как переводится слово «удиль»:",
+        "questionType": "text",
+        "answers": ["стерх", "лебедь", "чайка", "розовая чайка"],
+        "correctAnswer": "1"
+      }
+    ],
+    "quizTitle": "5"
+  }
+  
+  export const quizData6 = {
+    "questions": [
+      {
+        "question": "Из чего делают ярангу?",
+        "questionType": "text",
+        "answers": ["из жердей и ткани", "из жердей и оленьих шкур", "из оленьих шкур"],
+        "correctAnswer": "2"
+      },
+      {
+        "question": "Как переводится слово «строганина»:",
+        "questionType": "text",
+        "answers": ["альҕа", "тэльиэдальҕа", "чааха", "умуйэ"],
+        "correctAnswer": "3"
+      },
+      {
+        "question": "Как переводится слово «wадун нимэ»:",
+        "questionType": "text",
+        "answers": ["рыба", "юкола", "строганина", "яранга"],
+        "correctAnswer": "4"
+      }
+    ],
+    "quizTitle": "6"
+  }
+  
+  export const quizData7 = {
+    "questions": [
+      {
+        "question": "Пайпэн хандьэмэльэ сукун - это:",
+        "questionType": "text",
+        "answers": ["мужская зимняя национальная одежда", "женская зимняя национальная одежда", "женская летняя национальная одежда", "мужская летняя национальная одежда"],
+        "correctAnswer": "2"
+      },
+      {
+        "question": "Пайпэн лэwэймэльэ сукун - это:",
+        "questionType": "text",
+        "answers": ["женская летняя национальная одежда", "мужская летняя национальная одежда", "женская зимняя национальная одежда", "мужская зимняя национальная одежда"],
+        "correctAnswer": "1"
+      },
+      {
+        "question": "Кэйпэн хандьэмэльэ сукун - это:",
+        "questionType": "text",
+        "answers": ["женская зимняя национальная одежда", "женская летняя национальная одежда", "мужская летняя национальная одежда", "мужская зимняя национальная одежда"],
+        "correctAnswer": "4"
+      },
+      {
+        "question": "Как переводится слово «шарфик»:",
+        "questionType": "text",
+        "answers": ["угурчэ", "туурии", "ньамиирукун", "моҥо"],
+        "correctAnswer": "3"
+      }
+    ],
+    "quizTitle": "7"
+  }
+  
+  export const quizData8 = {
+    "questions": [
+      {
+        "question": "Как называется сказка, которую вы посмотрели?",
+        "questionType": "text",
+        "answers": ["«Почему северная куропатка белая?»", "«Почему у куропатки на лапах щётки?»", "«Почему у куропатки красные веки?»"],
+        "correctAnswer": "2"
+      },
+      {
+        "question": "Кто нашел куропатку?",
+        "questionType": "text",
+        "answers": ["девочка", "мальчик", "женщина"],
+        "correctAnswer": "3"
+      },
+      {
+        "question": "Что испачкала куропатка?:",
+        "questionType": "text",
+        "answers": ["крыло", "хвост", "клюв"],
+        "correctAnswer": "2"
+      }
+    ],
+    "quizTitle": "8"
+  }
+  
+  export const quizData9 = {
+    "questions": [
+      {
+        "question": "Илья Иванович Курилов:",
+        "questionType": "text",
+        "answers": ["юкагирский писатель", "юкагирский мелодист", "юкагирский художник"],
+        "correctAnswer": "2"
+      },
+      {
+        "question": "Переведите правильно отрывок песни «Лабунмэдэнудиэ» Лабунмэдэнудиэ, Льэр маархуонь мэт чуҥдэ.",
+        "questionType": "text",
+        "answers": ["Милая Лабунмэдэну, Моя мысль только о тебе.", "Милая Лабунмэдэну, Моя любовь к тебе огромна.", "Милая Лабунмэдэну, Я очень скучаю по тебе."],
+        "correctAnswer": "1"
+      },
+      {
+        "question": "Как переводится название песни «Мэт Йуорпурэ»:",
+        "questionType": "text",
+        "answers": ["«Моя жизнь»", "«Моя любовь»", "«Моя тундра»"],
+        "correctAnswer": "3"
+      },
+      {
+        "question": "Как переводится название песни «Мит эньиэ»:",
+        "questionType": "text",
+        "answers": ["«Моя бабушка»", "«Моя тундра»", "«Наша мама»"],
+        "correctAnswer": "3"
+      }
+    ],
+    "quizTitle": "9"
+  }
+  
+  export const quizData10 = {
+    "questions": [
+      {
+        "question": "Как зовут знаменитых братьев Куриловых?:",
+        "questionType": "text",
+        "answers": ["Гаврил, Семён, Василий", "Семён, Николай, Геннадий", "Семён, Гаврил, Николай"],
+        "correctAnswer": "3"
+      },
+      {
+        "question": "Кто написал роман «Ханидо и Халерха»?:",
+        "questionType": "text",
+        "answers": ["Семен Николаевич Курилов", "Гаврил Николаевич Курилов", "Николай Николаевич Курилов"],
+        "correctAnswer": "1"
+      },
+      {
+        "question": "Псевдоним Гаврила Николаевича Курилова:",
+        "questionType": "text",
+        "answers": ["Улуро чии", "Улуро Адо", "Окат Бэй"],
+        "correctAnswer": "2"
+      },
+      {
+        "question": "Псевдоним Николая Николаевича Курилова:",
+        "questionType": "text",
+        "answers": ["Окат Бэй", "Николай", "Улуро Адо"],
+        "correctAnswer": "1"
+      }
+    ],
+    "quizTitle": "10"
+  }
